7022850e317f7cc7c966fee9324d6031b5a3690d
Adison-Fryman <103378226+Adison-Fryman@users.noreply.github.com>
2023-01-20 13:03:21 -0500

    Create locators.py

---
+++ locators.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators(object):
+    # constant = access type,access point
+    GO_BUTTON = (By.ID, "submit")
+    # add more here
+
+
+class SearchResultsPageLocators(object):
+    pass
